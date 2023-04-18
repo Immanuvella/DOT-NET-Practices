da42a9100aece4cae470e911373a7e77b4594281
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-04-18 23:52:19 +0530

    Add files via upload

---
+++ Day12/CustomeException/CustomeException/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CustomeException
+
+{
+    //Creating our own Exception Class by inheriting Exception class
+    public class OddNumberException : Exception
+    {
+        //Overriding the Message property
+        public override string Message
+        {
+            get
+            {
+                return "Divisor Cannot be Odd Number";
+            }
+        }
+
+       
+       
+    }
+}
