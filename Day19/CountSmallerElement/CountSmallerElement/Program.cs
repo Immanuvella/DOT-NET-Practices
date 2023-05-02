33297558bef4468ed0923053fc8ab85e298a89e7
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-02 09:24:26 +0530

    Add files via upload

---
+++ Day19/CountSmallerElement/CountSmallerElement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSmallerElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Enter size of array");
+            N=int.Parse(Console.ReadLine());
+            int[] A = new int[N];
+            Console.WriteLine("Enter array elements");
+            for(int i=0;i<N;i++)
+            {
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            int res = Count(A, N);
+            Console.WriteLine(res);
+
+            Console.ReadKey();        }
+        static int Count(int[] A,int N)
+        {
+            int X;
+            Console.WriteLine("Enter a Number :");
+            X = int.Parse(Console.ReadLine());
+            int Count = 0;
+            for (int i=0;i<N;i++)
+            {
+                if (A[i]<=X)
+                {
+                    Count++;
+                }
+            }
+            return Count;
+        }
+    }
+}
