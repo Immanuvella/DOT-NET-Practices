99006a4156bf4944d7294a320b72ae3ff94861e6
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-02 17:03:48 +0530

    Add files via upload

---
+++ Day17/IsBinary/IsBinary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = Console.ReadLine(); // read the input string
+
+        bool result = IsBinary(str); // call the IsBinary function
+
+        Console.WriteLine(result); // output the result
+        Console.ReadKey();
+    }
+
+    public static bool IsBinary(string str)
+    {
+        foreach (char c in str)
+        {
+            if (c != '0' && c != '1')
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Day17/Requirement3/Requirement6/Program.cs
@@ -0,0 +1,46 @@
+﻿using Requirement3;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Requirement6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List <Songs> songs=new List<Songs> ();
+            int n;
+            Console.WriteLine("Enter number of songs :");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Details :");
+            for (int i=0;i<n;i++)
+            {
+                string[] str = Console.ReadLine().Split(',');
+                Songs song = new Songs(str[0], str[1], str[2], double.Parse(str[3]), int.Parse(str[4]), DateTime.Parse(str[5]));
+                songs.Add(song);
+            }         
+            Dictionary<string, int> typeCount = Songs.CalculateTypeCount(songs);
+           
+            Console.WriteLine("Song Type", "\tCount");
+            //foreach (KeyValuePair<string, int> pair in typeCount.OrderBy(x => x.Key))
+            //{
+                
+            //        Console.WriteLine(pair.Key, pair.Value);
+               
+            //}
+            
+            string StateofMind = Songs.PredictState(typeCount);
+            Console.WriteLine("The user is feeling "+StateofMind);
+
+        }
+
+        }
+}
+
