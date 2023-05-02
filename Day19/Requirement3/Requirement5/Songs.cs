a6b605eea09d39002285ce231b29035eefb58de8
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-02 17:00:10 +0530

    Add files via upload

---
+++ Day19/Requirement3/Requirement5/Songs.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Xml.Linq;
+
+namespace Requirement3
+{
+    public class Songs
+
+    {
+        private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		private string _artist;
+
+		public string Artist
+		{
+			get { return _artist; }
+			set { _artist = value; }
+		}
+		private string _songType;
+
+		public string SongType
+		{
+			get { return _songType; }
+			set { _songType = value; }
+		}
+		private double _rating;
+
+		public double Rating
+		{
+			get { return _rating; }
+			set { _rating = value; }
+		}
+		private int _numberOfDownloads;
+
+		public int NumberofDownloads
+		{
+			get { return _numberOfDownloads; }
+			set { _numberOfDownloads = value; }
+
+		}
+		private DateTime _dateDownloaded;
+
+		public DateTime DateDownloaded
+		{
+			get { return _dateDownloaded; }
+			set { _dateDownloaded = value; }
+		}
+        public Songs()
+        {
+
+        }
+
+        public Songs(string _name, string _artist, string _songType, double _rating, int _numberOfDownload, DateTime _dateDownloaded)
+        {
+            Name = _name;
+            Artist = _artist;
+            SongType = _songType;
+            Rating = _rating;
+            NumberofDownloads = _numberOfDownload;
+            DateDownloaded = _dateDownloaded;
+            
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} \t{1}  \t{2} \t{3} \t{4} \t\t\t\t{5}", Name, Artist, SongType, Rating, NumberofDownloads, DateDownloaded.ToString("dd-MM-yyyy", null));
+        }
+        public int CompareTo(Songs other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+        public static SortedDictionary<string, int> CalculateTypeCount(List<Songs> list)
+        {
+            SortedDictionary<string, int> typeCount = new SortedDictionary<string, int>();
+
+            foreach (Songs song in list)
+            {
+                if (!typeCount.ContainsKey(song.SongType))
+                {
+                    typeCount.Add(song.SongType, 1);
+                }
+                else
+                {
+                    typeCount[song.SongType]++;
+                }
+            }
+
+            Console.WriteLine("{0} {1,15}", "Song type", "Count");
+
+            foreach (KeyValuePair<string, int> pair in typeCount)
+            {
+                Console.WriteLine("{0} {1,15}", pair.Key, pair.Value);
+            }
+            return typeCount;
+        }
+    }
+
+
+
+
+       
+    }
+
+
+
+
+
+    
---
+++ Day19/Requirement3/Requirement6/Songs.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Xml.Linq;
+
+namespace Requirement3
+{
+	public class Songs
+
+	{
+		private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		private string _artist;
+
+		public string Artist
+		{
+			get { return _artist; }
+			set { _artist = value; }
+		}
+		private string _songType;
+
+		public string SongType
+		{
+			get { return _songType; }
+			set { _songType = value; }
+		}
+		private double _rating;
+
+		public double Rating
+		{
+			get { return _rating; }
+			set { _rating = value; }
+		}
+		private int _numberOfDownloads;
+
+		public int NumberofDownloads
+		{
+			get { return _numberOfDownloads; }
+			set { _numberOfDownloads = value; }
+
+		}
+		private DateTime _dateDownloaded;
+
+		public DateTime DateDownloaded
+		{
+			get { return _dateDownloaded; }
+			set { _dateDownloaded = value; }
+		}
+		public Songs()
+		{
+
+		}
+
+		public Songs(string _name, string _artist, string _songType, double _rating, int _numberOfDownload, DateTime _dateDownloaded)
+		{
+			Name = _name;
+			Artist = _artist;
+			SongType = _songType;
+			Rating = _rating;
+			NumberofDownloads = _numberOfDownload;
+			DateDownloaded = _dateDownloaded;
+
+		}
+		public override string ToString()
+		{
+			return string.Format("{0} \t{1}  \t{2} \t{3} \t{4} \t\t\t\t{5}", Name, Artist, SongType, Rating, NumberofDownloads, DateDownloaded.ToString("dd-MM-yyyy", null));
+		}
+		public int CompareTo(Songs other)
+		{
+			return Name.CompareTo(other.Name);
+		}
+
+		public static Dictionary<string, int> CalculateTypeCount(List<Songs> list)
+		{
+			var dict = new Dictionary<string, int>();
+			foreach (Songs s in list)
+			{
+				if (dict.ContainsKey(s._songType))
+				{
+					dict[s._songType]++;
+				}
+				else
+				{
+					dict.Add(s._songType, 1);
+				}
+			}
+			return dict;
+		}
+
+		public static string PredictState(Dictionary<string,int> perTypeCount)
+		{
+			int max = perTypeCount.Values.Max();
+			string maxType=perTypeCount.FirstOrDefault(x=>x.Value==max).Key;             
+				
+					if(maxType=="Emotional")
+					{
+                       return "Depressed";
+                    }
+					else if(maxType =="Celebration")
+					{
+                        return "Happy";
+                    }
+					else if(maxType=="Motivational")
+					{
+                        return "Energetic";
+                    }
+					else
+					{
+                        return "Unknown";
+                    }			
+        }	
+	}
+}
+
+
+
+
+
+
+
+    
