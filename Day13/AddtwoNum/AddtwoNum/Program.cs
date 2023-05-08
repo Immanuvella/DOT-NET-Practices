3681f22a9f754e866d0060e189b6158c955fabb4
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-08 10:30:36 +0530

    Add files via upload

---
+++ Day13/AddtwoNum/AddtwoNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddtwoNum
+{
+    internal class Program
+    { 
+        
+        public delegate int Sum(int x, int y);
+        static void Main(string[] args)
+        {
+            int result;
+            Sum s = delegate (int n, int m)
+            {
+                return n + m;
+            };
+            result = s(10, 20);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Day13/StreamWriterReader/StreamWriterReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriterReader
+{
+    class WriteAndReader
+    {
+        static string file = @"D:\UST\day13\file.txt";
+        static void Main(string[] args)
+        {
+            Write();
+            
+        }
+        static void Write()
+        {
+            StreamWriter fileStream = new StreamWriter(file);
+            fileStream.WriteLine("What is CallConvThiscall ");
+        }
+        static void Read()
+        {
+            StreamReader fileStream=new StreamReader(file);
+            string s = null;
+            while((s=fileStream.ReadLine()) != null) 
+            {
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
