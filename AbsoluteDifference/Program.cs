11ad748df50a388d4414cde9812998e6b31cfe73
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-08 10:02:48 +0530

    Add files via upload

---
+++ AbsoluteDifference/Program.cs
@@ -0,0 +1,37 @@
+﻿//program to find absolute difference
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsoluteDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a Number :");
+            n = int.Parse(Console.ReadLine());
+            int res1 = AbsoluteDiff(n);
+            Console.WriteLine("Result = "+res1);
+            Console.ReadKey();
+
+        }
+        static int AbsoluteDiff(int n)
+        {
+            if(n>51)//check the given number is  greater than 51 
+            {
+                int res = (n - 51)*3;
+                return res;//return result
+                
+            }
+            else
+            {
+                int res2 =( n - 51);
+                return res2;
+            }
+        }
+    }
+}
---
+++ Daily Practice Questions/Amstrong Number/Amstrong Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amstrong_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a Number:");
+            n=int.Parse(Console.ReadLine());
+            int res = AmstrongNumber(n);
+            if(res==n)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+        }
+        static int AmstrongNumber(int N)
+        {
+            int s = 0;
+            int rem = 0;
+            while(N>0)
+            {
+                rem = N % 10;
+                s = (rem * rem * rem) + s;
+                N = N / 10;
+                
+            }
+            return s;
+        }
+    }
+}
