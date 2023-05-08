bc76f5ec39460eef6e7d779ee2a9cad8d1ae78ec
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-08 09:38:47 +0530

    Add files via upload

---
+++ Day24/EFDemo1/EFDemo1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EMP_DBEntities1 e = new EMP_DBEntities1();
+            foreach(var item in e.Managers)
+            {
+                Console.WriteLine( item.Name);
+            }
+        }
+    }
+}
