5ef69d9dd2d819c170d053ca6ff6ce93633b7d26
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-05-29 09:24:51 +0530

    Add files via upload

---
+++ Day43/JS Practices/Practice.js
@@ -0,0 +1,12 @@
+function Age()
+{
+    let a=25;
+if(a>=18)
+
+    document.getElementById('change').innerHTML="Eligible to vote";
+
+
+else
+    document.getElementById('change').innerHTML="Not Eligible to vote";
+
+}
