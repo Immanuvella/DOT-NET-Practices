b4e0f5ad7b655f0eaf09fd38914d18df5c6ccb53
Immanuvella <129887773+Immanuvella@users.noreply.github.com>
2023-06-01 17:13:58 +0530

    Add files via upload

---
+++ Day44/Challenge2/Challenge2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> numbers = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string number = Console.ReadLine();
+            numbers.Add(number);
+        }
+
+        // Reorder the array
+        numbers.Sort();
+
+        // Output the reordered array
+        foreach (string number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
